0bbd6e79d33bdf96c0bda15a33394776a71c0528
VDK45 <vdk45blg@gmail.ru>
2021-01-29 19:15:15 +0900

    Результат : Nice Cool

--- Func_Task_6.py
+++ Func_Task_6.py
@@ -1,13 +1,22 @@
 def int_func():
     a = input().title().split()
     x = len(a)
+    y = []
     while x > 0:
         x -= 1
         for i_2 in a[x - 1]:
-            if ord(i_2) > 122:
-                a[x - 1].lower()
-                print(a[x - 1])
 
+            if ord(i_2) > 122:
+                y.append(x - 1)
+    y = set(y)
+    b = []
+    for x in y:
+        b.append(a[x])
+    a = set(a)
+    b = set(b)
+    a.difference_update(b)
+    a = list(a)
+    a = ' '.join(a)
     return a
 
 
